128137dee33d889c93297824fbac3d9733a4a994
amdlh <amhionicdeveloper@gmail.com>
2022-11-07 16:59:23 +0100

    New styling

--- src/components/Puzzle.jsx
+++ src/components/Puzzle.jsx
@@ -1,6 +1,7 @@
 import { useReducer } from "react";
 import { INITIAL_STATE } from "../constants/initialState";
 import { reducer } from "../reducer";
+import Confetti from "react-confetti";
 
 import "./Puzzle.css";
 
@@ -11,35 +12,41 @@ const Puzzle = () => {
 
   return (
     <div className="Puzzle">
-      <div className="Puzzle-squares">
-        {state.items.map((s, i) => (
-          <div
-            className={`Puzzle-square ${s ? "" : "Puzzle-square-empty"}`}
-            key={`square-${i}`}
-            onClick={() => dispatch({ type: "move", payload: i })}
-          >
-            {s}
-          </div>
-        ))}
-      </div>
+      <div>
+        <div className="Puzzle-squares">
+          {state.items.map((s, i) => (
+            <div
+              className={`Puzzle-square ${s ? "" : "Puzzle-square-empty"}`}
+              key={`square-${i}`}
+              onClick={() => dispatch({ type: "move", payload: i })}
+            >
+              {s}
+            </div>
+          ))}
+        </div>
 
-      <div className="Puzzle-controls">
-        <button className="Puzzle-shuffle" onClick={() => dispatch({ type: "shuffle" })}>
-          Shuffle
-        </button>
-        <button className="Puzzle-reset" onClick={() => dispatch({ type: "reset" })}>
-          Reset
-        </button>
+        <div className="Puzzle-controls">
+          <button
+            className="button-85 reset-button"
+            role="button"
+            onClick={() => dispatch({ type: "shuffle" })}
+          >
+            Shuffle
+          </button>
+          <button className="button-85 shuffle-button" onClick={() => dispatch({ type: "reset" })}>
+            Reset
+          </button>
+        </div>
+        <div className="Puzzle-controls">
+          <button className="Puzzle-undo" onClick={() => dispatch({ type: "undo" })}>
+            Undo
+          </button>
+          <button className="Puzzle-redo" onClick={() => dispatch({ type: "redo" })}>
+            Redo
+          </button>
+        </div>
+        {state.complete && <Confetti />}
       </div>
-      <div className="Puzzle-controls">
-        <button className="Puzzle-undo" onClick={() => dispatch({ type: "undo" })}>
-          Undo
-        </button>
-        <button className="Puzzle-undo" onClick={() => dispatch({ type: "redo" })}>
-          Redo
-        </button>
-      </div>
-      {state.complete && <div className="Puzzle-complete">Complete!</div>}
     </div>
   );
 };
